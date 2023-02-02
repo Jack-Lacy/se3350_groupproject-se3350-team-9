20e8cc9864484c0a9c2883be70af46f9fe3ad889
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-02-02 18:50:46 -0500

    started code for the express backend

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,15 @@
-const mongoose = require('mongoose');
-const { collection, find } = require('../../database/schema/genre');
-require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+const port = 3000;
 
-const uri = `mongodb+srv://mtuenmuk:${process.env.password}@se3316.cfpaiqb.mongodb.net/se3316?retryWrites=true&w=majority`;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => console.log('connected to db'))
-    .catch((err) => console.log(err))
+const app = express();
+app.use(cors());
+app.use(express.json())
 
-function saveData(document){
-    document.save()
-};
+app.get('/', (req,res) => {
+    res.send("SE3350 Project")
+})
 
-function delData(collection){
-    collection.remove()
-    console.log("removed")
-};
-
-function getAll(collection){
-    return collection.find({})
-}
-
-function findAll(collection, param, value){
-    return collection.find({
-        [param]: value
-    })
-}
-
-function findOne(collection, param, value, updateKey, updateValue){
-    let item = collection.findOne({
-        [param]: value
-    })
-    
-    item.updateKey = updateValue;
-
-    const doc = new collection(item)
-
-    doc.save();
-
-    return item
-}
-
-function deleteAll(collection){
-    collection.deleteMany({})
-}
-
-function deleteOne(collection, param, value){
-    collection.deleteOne({[param]: value});
-}
-
-function checkExist(collection, param, check){
-    return collection.exists({
-        [param]: check
-    })
-}
-
-module.exports = {saveData, delData, findAll, deleteAll, checkExist, findOne, deleteOne};
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+})
