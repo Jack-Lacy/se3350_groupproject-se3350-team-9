f5f4c293c0fa0b71adc3abae57e28f772aefa2a4
Michael Yik <Michael.yik7@gmail.com>
2023-02-02 19:23:12 -0500

    created Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom"
+//importing react
+import React from 'react'
+function Header (){
+    return (
+        <div>
+            <h1>
+                ECE Course Outline Web-App
+            </h1>
+
+            <h4> Options </h4>
+            <nav>
+                <Link to = "/">Login</Link> |{"  "}
+                <Link to = "/home">Home</Link>|{"  "}
+            </nav>
+        
+        </div>
+    )
+};
+
+export default Header;
