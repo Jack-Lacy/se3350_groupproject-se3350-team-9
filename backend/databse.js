baa0021c4c024b7e7ca6d9199172dd6f79d225e2
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-02-02 19:43:38 -0500

    changed the uri to be the correct one

--- backend/databse.js
+++ backend/databse.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { collection, find } = require('../../database/schema/genre');
 require("dotenv").config();
 
-const uri = `mongodb+srv://mtuenmuk:${process.env.password}@se3316.cfpaiqb.mongodb.net/se3316?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://root:root@se3350.y4hv9vg.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => console.log('connected to db'))
     .catch((err) => console.log(err))
