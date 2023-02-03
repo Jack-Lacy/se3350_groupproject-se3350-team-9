08a85ad8ea0bc55ffcbe651d49dbf5407bbe197a
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-02-02 19:39:28 -0500

    added comments for the database code

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require("cors");
 const port = 3000;
 
+const {saveData, findAll, checkExist, update, deleteOne, findOneStr} = require('./database.js');
+
+
 const app = express();
 app.use(cors());
 app.use(express.json())
