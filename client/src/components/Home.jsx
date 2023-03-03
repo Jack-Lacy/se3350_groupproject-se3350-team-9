bb19c16573e730dbd69628a600fe1b52232c6e98
Jack-Lacy <jlacy@uwo.ca>
2023-03-02 20:02:59 -0500

    added a simple text editor
    no formatting functionality yet

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header"
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 // importing Link from react-router-dom to navigate to
 // different end points.
@@ -21,6 +23,9 @@ function Home  (){
 
                 </Header>
             </h1>
+            <ReactQuill
+            
+            />
         </div>
     )
 
